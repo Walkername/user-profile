9bd6b3595eb7a23db1af33a4cfcf9245f56884a6
Walkername <05Timur51@gmail.com>
2025-02-11 14:46:09 +0300

    feat(users): changed dto for updating, added checking(token_id, current_id) in user update endpoint

--- src/main/java/ru/walkername/user_profile/dto/UserDTO.java
+++ src/main/java/ru/walkername/user_profile/dto/UserDTO.java
@@ -1,17 +1,15 @@
 package ru.walkername.user_profile.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
-
 public class UserDTO {
 
-    @NotEmpty(message = "Nickname should not be empty")
-    @Size(min = 5, max = 20, message = "Nickname should be greater than 5 and less than 20 characters")
     private String username;
 
-    @NotEmpty(message = "Password cannot be empty")
-    @Size(min = 5, message = "Password should be greater than 5 characters")
-    private String password;
+    private String description;
+
+    public UserDTO(String username, String description) {
+        this.username = username;
+        this.description = description;
+    }
 
     public String getUsername() {
         return username;
@@ -21,11 +19,11 @@ public class UserDTO {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getDescription() {
+        return description;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
